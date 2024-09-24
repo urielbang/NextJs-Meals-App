9812ef2962aacfc2987edd96cbfa5e873d48c09e
urielbang <uri32655@gmail.com>
2024-09-24 11:09:16 +0300

    start meals app

--- app/about/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AboutPage() {
-  return (
-    <main>
-      <h1>About us</h1>
-    </main>
-  );
-}
--- app/blog/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BlogPostPage() {
-  return (
-    <main>
-      <h1>Blog Post</h1>
-    </main>
-  );
-}
