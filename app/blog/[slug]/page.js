13d5f1db0362f02858a3b59edd34260a6ab542b4
urielbang <uri32655@gmail.com>
2024-09-24 11:01:28 +0300

    add dinamic pages

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function BlogPostPage() {
+  return (
+    <main>
+      <h1>Blog Post</h1>
+    </main>
+  );
+}
---
+++ components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Welcome to this NextJS Course!</h1>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+    </>
+  );
+}
