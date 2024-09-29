cb51a153d25e1b3e7ceab61eac0361895e1a850b
urielbang <uri32655@gmail.com>
2024-09-29 15:36:44 +0300

    add db sql

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import React from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +20,9 @@ export default function MealsPage() {
           </p>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   );
 }
