3aa8543d60c8694581975dbbede3653a4a538000
urielbang <uri32655@gmail.com>
2024-09-30 12:35:01 +0300

    add not-found page and error page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,5 +1,30 @@
 import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
 
-export default function page({ params }) {
-  return <h1>{params.slug}</h1>;
+export default function MealDetailsPage({ params }) {
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>Title</h1>
+          <p className={classes.creator}>
+            by <a href={` mailto: %{Email}`}>Name</a>
+          </p>
+          <p className={classes.summary}>Summary</p>
+        </div>
+      </header>
+      <min>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: "...",
+          }}
+          className={classes.instructions}
+        />
+      </min>
+    </>
+  );
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import MealsLoadingPage from "./loading-out";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +25,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
