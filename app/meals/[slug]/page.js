caae7bba4284575dfef886703218b4c4229f9bbf
urielbang <uri32655@gmail.com>
2024-10-31 18:51:07 +0200

    test3

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -31,20 +31,20 @@ export default async function MealDetailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill src={meal.image} alt={meal.title} />
+          <Image fill src={meal?.image} alt={meal?.title} />
         </div>
         <div className={classes.headerText}>
-          <h1>{meal.title}</h1>
+          <h1>{meal?.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+            by <a href={`mailto: ${meal?.creator_email}`}>{meal?.creator}</a>
           </p>
-          <p className={classes.summary}>{meal.summary}</p>
+          <p className={classes.summary}>{meal?.summary}</p>
         </div>
       </header>
       <min>
         <p
           dangerouslySetInnerHTML={{
-            __html: meal.instructions,
+            __html: meal?.instructions,
           }}
           className={classes.instructions}
         />
